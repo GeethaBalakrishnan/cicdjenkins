46dbff0eb6ea01660b9a5aac704e1b5093bba050
= <=>
2022-03-10 08:59:02 +0000

    modified

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project created by Geetha Balakrishnan";
+        return "DevOps CICD Project created by Geetha Balakrishnan";
     }
 }
